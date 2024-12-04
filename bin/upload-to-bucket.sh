6fc2ef6375890d8de1aa44fbe5a3821d3efd7f47
Emmanuel Urias <emmanuelurias60@icloud.com>
2024-12-04 13:09:02 -0800

    code update

--- bin/upload-to-bucket.sh
+++ bin/upload-to-bucket.sh
@@ -1,4 +1,5 @@
 zip -r9 paypal-processor.zip index.py
+zip -r9 layer.zip python
 
 aws s3 cp paypal-processor.zip s3://rcw-code-bucket/paypal-processor/paypal-processor.zip
 aws s3 cp layer.zip s3://rcw-code-bucket/paypal-processor/layer.zip
